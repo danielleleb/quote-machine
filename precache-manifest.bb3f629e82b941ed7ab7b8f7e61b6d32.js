f44e28dc198319cecb0178c62371a7515fc8b3af
Danielle Leblanc-Cyr <d.leblanc-cyr@creating-web.com>
2018-12-04 08:57:27 +0100

    Updates

---
+++ precache-manifest.bb3f629e82b941ed7ab7b8f7e61b6d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22742a60cdb878f6f135",
+    "url": "/quote-machine/static/js/runtime~main.22742a60.js"
+  },
+  {
+    "revision": "30c4f23209f1b0c9691b",
+    "url": "/quote-machine/static/js/main.30c4f232.chunk.js"
+  },
+  {
+    "revision": "4b4c72de8b94465c18ad",
+    "url": "/quote-machine/static/js/1.4b4c72de.chunk.js"
+  },
+  {
+    "revision": "30c4f23209f1b0c9691b",
+    "url": "/quote-machine/static/css/main.c774e520.chunk.css"
+  },
+  {
+    "revision": "dd6d36fc123cb4a0ad6dbcf6fdd2e3b8",
+    "url": "/quote-machine/index.html"
+  }
+];
