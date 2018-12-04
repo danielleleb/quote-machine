91c4d1cb5cbe9b645eaed625c61ddedd0109e780
Danielle Leblanc-Cyr <d.leblanc-cyr@creating-web.com>
2018-12-04 09:19:46 +0100

    adds styling

--- src/components/TweetQuote.js
+++ src/components/TweetQuote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TweetQuote = (props) => {
     return (
-        <a href="https://twitter.com/intent/tweet" id="tweet-quote">tweet</a>
+        <a href="https://twitter.com/intent/tweet" id="tweet-quote">Tweet</a>
     )
 }
 
--- src/components/quote.js
+++ src/components/quote.js
@@ -4,8 +4,8 @@ import React, {Component} from 'react';
 const Quote = (props) => {
     return(
         <div className='quote'>
-            <div id="text">
-                {props.quote.data[0].content}
+            <div id="text" dangerouslySetInnerHTML={{__html: props.quote.data[0].content}}>
+                {/*{props.quote.data[0].content}*/}
             </div>
             <div id="author">
                 {props.quote.data[0].title}
